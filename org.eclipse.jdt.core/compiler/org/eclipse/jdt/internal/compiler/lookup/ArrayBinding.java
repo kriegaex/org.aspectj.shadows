9c99c39e4a2e7e722cb3c915162341948467fe10
Andy Clement <aclement@pivotal.io>
2016-11-04 17:39:16 -0700

    ensure protected removed from clone method

--- org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/ArrayBinding.java
+++ org.eclipse.jdt.core/compiler/org/eclipse/jdt/internal/compiler/lookup/ArrayBinding.java
@@ -501,7 +501,11 @@ public MethodBinding getCloneMethod(final MethodBinding originalMethod) {
 			return originalMethod.signature(); // for codeGen we need to answer the signature of j.l.Object.clone()
 		}
 	};
-	method.modifiers = originalMethod.modifiers;
+	// AspectJ was:
+	// method.modifiers = originalMethod.modifiers;
+	// now:
+	method.modifiers = (originalMethod.modifiers & ~ClassFileConstants.AccProtected) | ClassFileConstants.AccPublic;
+	// End AspectJ
 	method.selector = originalMethod.selector;
 	method.declaringClass = originalMethod.declaringClass; // cannot set array binding as declaring class, will be tweaked in CodeStream.getConstantPoolDeclaringClass()
 	method.typeVariables = Binding.NO_TYPE_VARIABLES;
